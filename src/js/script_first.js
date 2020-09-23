6fea9f1ce86f7e3597ff87f85c8f6a441e844745
Lorsten <olof126@outllok.com>
2020-09-23 19:51:25 +0200

    Initial design for  a financial advice website using sass for styling Author olof Andersson

--- src/js/script_first.js
+++ src/js/script_first.js
@@ -1,2 +1,26 @@
 
-console.log("First javascript file");
+window.onload = () =>{
+    let menu = document.querySelector('nav');
+    menu.style.right = '-110%';
+
+}
+document.getElementById("hamburger-menu").addEventListener("click", function(){
+    let menu = document.querySelector('nav');
+    this.style.zIndex = 10;
+
+    if(menu.style.right === '0%'){
+        this.className ="hamburger hamburger--collapse";
+        menu.style.right = '-110%';
+        this.style.zIndex = 0;
+        this.style.right ="10%";
+        this.style.position = 'absolute';
+    }
+    else{
+        this.className ="hamburger hamburger--collapse is-active";
+        menu.style.display ="block";
+        menu.style.right = '0%';
+        this.style.position = 'fixed';
+        this.style.right = '15vw';
+        this.style.top ='0';
+    }
+})
